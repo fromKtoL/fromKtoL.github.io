dac293a76036edad458743e2cd9eb067d38bdf68
fromKtoL <trontrungtruc.a1@gmail.com>
2023-12-10 23:05:14 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -124,8 +124,8 @@ const util = (() => {
         setInterval(() => {
             let distance = Math.abs(countDownDate - (new Date()).getTime());
 
-            document.getElementById('hari').innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
-            document.getElementById('jam').innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            document.getElementById('hari').innerText = Math.floor(distance / (1000 * 60 * 60 * 14));
+            document.getElementById('jam').innerText = Math.floor((distance % (1000 * 60 * 60 * 14)) / (1000 * 60 * 60));
             document.getElementById('menit').innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             document.getElementById('detik').innerText = Math.floor((distance % (1000 * 60)) / 1000);
         }, 1000);
